c2397fe25735eefdebc77f100a6556e663fb158a
Terexes <rafamoreci@gmail.com>
2022-01-11 19:37:53 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const login = document.querySelector('.input-login');
 const senha = document.querySelector('.input-senha');
 const entrar = document.querySelector('.btn-entrar');
 const submitButton = document.querySelector('#submit-btn');
+const textarea = document.getElementById('textarea');
+const contadorPalavras = document.getElementById('counter');
 
 function verificarLogin() {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -11,17 +13,24 @@ function verificarLogin() {
   }
 }
 
+function displaySubmit() {
+  if (submitButton.disabled == false) {
+    submitButton.disabled = true;
+    submitButton.style.display = 'none';
+  } else if (submitButton.disabled == true) {
+    submitButton.disabled = false;
+    submitButton.style.display = 'flex';
+  }
+}
+
+function contador() {
+  let counter = 500;
+  contadorPalavras.innerHTML = counter - textarea.value.length;
+}
+
 entrar.addEventListener('click', function (e) {
   e.preventDefault();
   verificarLogin();
 });
 
-function displaySubmit() {
-    if (submitButton.disabled == false) {
-        submitButton.disabled = true;
-        submitButton.style.display = 'none';
-    } else if (submitButton.disabled == true) {
-        submitButton.disabled = false;
-        submitButton.style.display = 'flex';
-    }
-}
+textarea.addEventListener('keyup', contador);
